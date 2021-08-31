d9d00a6f919732479e7648f29775c68dc7d26ab2
Oleg Bydlovskiy <olezuk000416@gmail.com>
2021-09-01 02:05:44 +0300

    make regExp and functions of validation SignUp form

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-'use strict'
+'use strict'  
+// regExp
+const upForm = document.forms.signUp;
+const inForm = document.forms.signIn;
+
+// SIGN UP VALIDATION
+
+let firstNameExp = /^[a-zA-z]{2,}$/
+let lastNameExp = /^[a-zA-z]{2,}$/
+let emailExp = /^\w+@\w+\.\w+$/;
+let passwordExp = /^\w{4,15}$/;
+
+
+function validation(regExp,elem){
+    if(regExp.test(upForm[elem].value)){
+        upForm[elem].classList.add('is-valid')
+        return true
+    } else {
+        upForm[elem].classList.add('is-invalid')
+    }
+}
+
+function runValidation(){
+    validation(firstNameExp,'firstName');
+    validation(lastNameExp,'lastName');
+    validation(emailExp,'email');
+    validation(passwordExp,'password');
+}
+
+function checkForm(){
+    runValidation();
+    if(validation(firstNameExp,'firstName') && validation(lastNameExp,'lastName') && validation(emailExp,'email') && validation(passwordExp,'password')){
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+}
+
+upForm.addEventListener('submit',function(event){
+    event.preventDefault();
+    checkForm();
+})
+
+
+
+
+
+
+
+
+
